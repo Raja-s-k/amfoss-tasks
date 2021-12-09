a5d428fbbd1585078cc32936938e333c4e879da9
Raja-s-k <rajask42042@gmail.com>
2021-12-09 19:53:39 +0530

    advent-of-code

---
+++ advent-of-code/day-02/code1.py
@@ -0,0 +1,18 @@
+a=open("inputdata.txt","r")
+b = a.read()
+b = b.splitlines()
+c=0
+d=0
+
+for i in b:
+    f=i.split(" ")
+    if f[0] == "forward":
+        c+=int(f[1])
+    elif f[0] == "down":
+        d+=int(f[1])
+    elif f[0] == "up":
+        d-=int(f[1])
+    else:
+        pass
+
+print(c*d)
---
+++ advent-of-code/day-02/code2.py
@@ -0,0 +1,20 @@
+a=open("inputdata.txt","r")
+b = a.read()
+b = b.splitlines()
+c=0
+d=0
+f=0
+
+for i in b:
+    g=i.split(" ")
+    if g[0] == "forward":
+        c+=int(g[1])
+        d+=f*int(g[1])
+    elif g[0] == "down":
+        f+=int(g[1])
+    elif g[0] == "up":
+        f-=int(g[1])
+    else:
+        pass
+
+print(c*d)
