31598ecd62150a8d6880ef7e37e768f200cf9717
Raja-s-k <rajask42042@gmail.com>
2022-03-14 06:29:07 +0530

    advent-of-code

--- advent-of-code/day-01/code2.py
+++ advent-of-code/day-01/code2.py
@@ -1,8 +1,8 @@
 a=open("inputdata.txt","r")
 b = a.read()
 b = b.splitlines()
-c=int(b[0])+int(b[1])+int(b[2])
-d=0
+c = int(b[0])+int(b[1])+int(b[2])
+d = 0
 
 for i in range(1998):
     i=int(b[i])+int(b[i+1])+int(b[i+2])
